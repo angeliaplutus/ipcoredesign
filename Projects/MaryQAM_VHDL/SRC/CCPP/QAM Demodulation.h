1204c7ff45ba540fe1ae0d0d4cadd18dc56071e3
Angelia <63243602+angeliaplutus@users.noreply.github.com>
2023-10-02 19:15:43 +0800

    Add files via upload
    Signed-off-by: Angelia <63243602+angeliaplutus@users.noreply.github.com>

---
+++ Projects/MaryQAM_VHDL/SRC/CCPP/QAM Demodulation.h
@@ -0,0 +1,15 @@
+#include<vector>
+#include<math.h>
+using namespace std;
+
+// demodulate voltage into constellation points represented by grey code
+vector<vector<int>> QAMdemod(vector<vector<double>> I, vector<vector<double>> Q, int modulationOrder);
+
+// normalize each row in I & Q
+void normalize(vector<vector<double>> &I, vector<vector<double>> &Q);
+
+// get grey codec according to index
+int greyEncode(int idx, int order);
+
+// the max function
+inline double max(double x, double y);
